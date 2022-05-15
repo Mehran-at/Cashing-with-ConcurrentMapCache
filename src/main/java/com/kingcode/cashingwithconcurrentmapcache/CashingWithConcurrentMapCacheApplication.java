88b3282f007e6234a1673298dca46246c36b0099
Mehran Hosseini <mehran.hosseini@ebcont.com>
2022-05-15 20:05:13 +0200

    init commit

---
+++ src/main/java/com/kingcode/cashingwithconcurrentmapcache/CashingWithConcurrentMapCacheApplication.java
@@ -0,0 +1,13 @@
+package com.kingcode.cashingwithconcurrentmapcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashingWithConcurrentMapCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashingWithConcurrentMapCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kingcode/cashingwithconcurrentmapcache/CashingWithConcurrentMapCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingcode.cashingwithconcurrentmapcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashingWithConcurrentMapCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
